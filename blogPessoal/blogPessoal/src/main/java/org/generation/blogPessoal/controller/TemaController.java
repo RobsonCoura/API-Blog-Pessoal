03f4560c11c2e3e5e46e25a173143dcded3390a4
Robson Coura <robson_coura@yahoo.com.br>
2025-02-20 14:53:50 -0300

    feat:(TemaController) Criando endpoint para buscar tema por ID

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/TemaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,5 +23,11 @@ public class TemaController {
     @GetMapping
     public ResponseEntity<List<Tema>> getAll() {
         return ResponseEntity.ok(repository.findAll());
+    }
+
+    // Metodo para buscar tema por ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Tema> getById(@PathVariable long id) {
+        return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
     }
 }
