48cb70c2a2c6c42018da08b2b0046fcf7b89e67a
Robson Coura <robson_coura@yahoo.com.br>
2025-02-20 14:28:17 -0300

    feat:(TemaRepositoy) Método para buscar todos os temas

---
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/repository/TemaRepository.java
@@ -0,0 +1,15 @@
+package org.generation.blogPessoal.repository;
+
+import org.generation.blogPessoal.model.Tema;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TemaRepository extends JpaRepository<Tema, Long> {
+
+    // Metodo que busca todos os temas
+    public List<Tema> findAllByDescricaoContainingIgnoreCase(String descricao);
+
+}
