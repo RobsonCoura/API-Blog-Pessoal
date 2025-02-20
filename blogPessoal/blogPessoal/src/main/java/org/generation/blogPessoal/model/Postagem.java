6d411d18e4b15d33025eaeb3fe4b750a29128c93
Robson Coura <robson_coura@yahoo.com.br>
2025-02-20 14:15:22 -0300

    feat:(Postagem) Criando relacionamento da entidade Postagem com a Tema

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -1,5 +1,6 @@
 package org.generation.blogPessoal.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import org.antlr.v4.runtime.misc.NotNull;
@@ -25,6 +26,9 @@ public class Postagem {
     @Temporal(TemporalType.TIMESTAMP)
     private Date date = new Date(System.currentTimeMillis());
 
+    @ManyToOne
+    @JsonIgnoreProperties("postagem")
+    private Tema tema;
 
     // Getters e Setters
     public long getId() {
@@ -57,6 +61,14 @@ public class Postagem {
 
     public void setDate(Date date) {
         this.date = date;
+    }
+
+    public Tema getTema() {
+        return tema;
+    }
+
+    public void setTema(Tema tema) {
+        this.tema = tema;
     }
 
 }
